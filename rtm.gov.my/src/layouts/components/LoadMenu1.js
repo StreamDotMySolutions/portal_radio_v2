e3b474928b1e74b2641343616814b13f32d2f226
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-09 16:02:38 +0800

    [RTM.GOV.MY] multi columns if more than 8 articles

--- rtm.gov.my/src/layouts/components/LoadMenu1.js
+++ rtm.gov.my/src/layouts/components/LoadMenu1.js
@@ -132,9 +132,8 @@ function LoadMenu1({id}) {
             )}
           </a>
 
-          <div className={articles.length < 7 ? "dropdown-menu" : "dropdown-menu"} style={articles.length < 7 ? { minWidth: '320px' } : {}}>
+          <div className="dropdown-menu" style={articles.length < 8 ? { minWidth: '320px' } : {}}>
         
-  
             <div className='row ml-2'>  
               {items()}
             </div>
