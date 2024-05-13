7a265ed71aadf5406629c4901b66d70eb37a655d
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-13 15:21:17 +0800

    [RTM.GOV.MY] Menu2 SingleArticle fix

--- rtm.gov.my/src/layouts/ListingLayout/SingleArticle.js
+++ rtm.gov.my/src/layouts/ListingLayout/SingleArticle.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import PageContent from '../ContentLayout/components/PageContent';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
 const SingleArticle = ({id}) => {
+    //const { id } = useParams(); // parentid
+    const [items, setItems] = useState([]);
+    const [ancestors, setAncestors] = useState([]);
+    const [title, setTitle] = useState('');
+    const [loading, setLoading] = useState(true);
+    const url = process.env.REACT_APP_API_URL;
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+    useEffect(() => {
+        axios(`${url}/show/${id}`)
+            .then(response => {
+                //console.log(response)
+                setTitle(response.data.title);
+                setItems(response.data.items);
+                setAncestors(response.data.ancestors);
+                setLoading(false); // Set loading to false when data is fetched
+            })
+            .catch(error => {
+                console.warn(error);
+                setLoading(false); // Set loading to false on error as well
+            });
+    }, [id]);
+
+    const contentItems = () => {
+        return items.map((item, index) => (
+            <div className='mb-2' key={index} dangerouslySetInnerHTML={{ __html: item.contents }} />
+        ));
+    };
+
     return (
-       <PageContent id={id} />
+
+        <div className="container-fluid" style={{ "marginTop": "4rem" }}>
+            {loading ? (
+                <></>
+            ) : (
+                <>{contentItems()}</>
+            )}
+        </div>
+
     );
 };
 
