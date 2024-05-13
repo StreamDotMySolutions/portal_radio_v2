73d3ac974362fab3ab35f437d66da4ad5e791ea3
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-13 15:58:17 +0800

    [RTM.GOV.MY] change show breadcrumb depth

--- rtm.gov.my/src/layouts/components/LoadMenu1.js
+++ rtm.gov.my/src/layouts/components/LoadMenu1.js
@@ -59,59 +59,6 @@ function LoadMenu1({id}) {
 
   
 
-  // const items = () => {
-  //   if (articles.length === 0) {
-  //     return null; // or any default content you want to render when there are no articles
-  //   }
-  
-  //   return articles.map(article => (
-  //     <li key={article.id} className="nav-item ml-2">
-  //       {article.article_setting && article.article_setting.redirect_url ? (
-  //         <NavLink to={article.article_setting.redirect_url} className="nav-link">
-  //           {article.title}
-  //         </NavLink>
-  //       ) : (
-  //         <NavLink to={`/contents/${article.id}`} className="nav-link">
-  //           {article.title}
-  //         </NavLink>
-  //       )}
-  //     </li>
-  //   ));
-  // };
-
-  // const items = () => {
-  //   if (articles.length === 0) {
-  //     return null; // or any default content you want to render when there are no articles
-  //   }
-  
-  //   const groupedArticles = [];
-  //   for (let i = 0; i < articles.length; i += 2) {
-  //     groupedArticles.push(articles.slice(i, i + 2));
-  //   }
-  
-  //   return groupedArticles.map((group, index) => (
-
-      
-  //     <div className="col-md-4" >
-  //       <span style={{"color":"#ffa525"}}>{title}</span>
-  //         <ul className='nav flex-column'>
-  //           <li key={group.id} className="nav-item border-right">
-  //             {group.map(article => (
-  //               <NavLink
-  //                 key={article.id}
-  //                 to={article.article_setting && article.article_setting.redirect_url ? article.article_setting.redirect_url : `/contents/${article.id}`}
-  //                 className="dropdown-item"
-  //               >
-  //                 {article.title}
-  //               </NavLink>
-  //             ))}
-  //           </li>
-  //         </ul>
-  //      </div> 
-  //   ));
-  // };
-  
-
   const items = () => {
     if (articles.length === 0) {
       return null; // or any default content you want to render when there are no articles
@@ -122,8 +69,6 @@ function LoadMenu1({id}) {
       groupedArticles.push(articles.slice(i, i + 8));
     }
 
-
-  
     return groupedArticles.map((group, index) => (
       <div key={index} className={`col-md-3 ${index === 1 ? 'ml-5' : 'mr-5'}`}>
 
