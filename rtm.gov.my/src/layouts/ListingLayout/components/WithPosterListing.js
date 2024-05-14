de11aba09b3bb5d30f57adbd31dd6e11d7834122
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-14 08:35:06 +0800

    [RTM] WithPosterListing convert to DIV

--- rtm.gov.my/src/layouts/ListingLayout/components/WithPosterListing.js
+++ rtm.gov.my/src/layouts/ListingLayout/components/WithPosterListing.js
@@ -9,42 +9,90 @@ const WithPosterListing = ({items}) => {
     const path = `${serverUrl}/storage/article_poster`
   
 
-    const contentItems = () => {
-        return items.map((item, index) => (
+    // const contentItems = () => {
+    //     return items.map((item, index) => (
 
-        <tr key={index}> 
-            <td style={{ width: '300px', verticalAlign: 'middle' }}>
-                {item.article_poster ?
-                    <img className='img-fluid img-thumbnail' src={`${path}/${item.article_poster.filename}`} />
-                    :
-                    <img className='img-fluid img-thumbnail' src={PlaceholderImage} />
-                }
-            </td>
-            <td className='p-3'>
+    //     <tr key={index}> 
+    //         <td style={{ width: '300px', verticalAlign: 'middle' }}>
+    //             {item.article_poster ?
+    //                 <img className='img-fluid img-thumbnail' src={`${path}/${item.article_poster.filename}`} />
+    //                 :
+    //                 <img className='img-fluid img-thumbnail' src={PlaceholderImage} />
+    //             }
+    //         </td>
+    //         <td className='p-3'>
 
-                <h5 className='display-5'>{item.title}</h5>
-                <p className='lead'>{item.description}</p>
+    //             <h5 className='display-5'>{item.title}</h5>
+    //             <p className='lead'>{item.description}</p>
                 
-                <Button style={{backgroundColor:'orange', color:'black', fontWeight: 'normal'}} className="float-right">
-    <Link to={`/contents/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-        {item.title} &raquo;
-    </Link>
-</Button>
-            </td>
-        </tr>
+    //             <Button style={{backgroundColor:'orange', color:'black', fontWeight: 'normal'}} className="float-right">
+    //                 <Link to={`/contents/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+    //                     {item.title} &raquo;
+    //                 </Link>
+    //             </Button>
+    //         </td>
+    //     </tr>
 
-        ));
+    //     ));
+    // };
+    const contentItems = () => {
+        return (
+            <div className="row">
+                {items.map((item, index) => (
+                    <div className="col-lg-6 col-md-12" key={index}>
+                        <div className="card mb-4">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-md-4">
+                                        {item.article_poster ? (
+                                            <img
+                                                className="img-fluid img-thumbnail"
+                                                src={`${path}/${item.article_poster.filename}`}
+                                                alt={item.title}
+                                            />
+                                        ) : (
+                                            <img
+                                                className="img-fluid img-thumbnail"
+                                                src={PlaceholderImage}
+                                                alt="Placeholder"
+                                            />
+                                        )}
+                                    </div>
+                                    <div className="col-md-8">
+                                        <h5 className="card-title">{item.title}</h5>
+                                        <p className="card-text">{item.description}</p>
+                                        <Button
+                                            style={{ backgroundColor: 'orange', color: 'black', fontWeight: 'normal' }}
+                                            className="float-right"
+                                        >
+                                            <Link
+                                                to={`/contents/${item.id}`}
+                                                style={{ textDecoration: 'none', color: 'inherit' }}
+                                            >
+                                                {item.title} &raquo;
+                                            </Link>
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
     };
+    
 
 
     return (
         <div>
-           <table class="table border-0" id="table-id">
+            {contentItems()}
+           {/* <table class="table border-0" id="table-id">
            
                 <tbody>
                     {contentItems()}
                 </tbody>
-            </table>
+            </table> */}
         </div>
     );
 };
