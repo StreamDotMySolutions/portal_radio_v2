022507388f2959a80f5719fa27523b68f92e3168
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-15 08:57:33 +0800

    [API/RTM] BannerController remove published_start / end check

--- api/app/Http/Controllers/Frontend/BannerController.php
+++ api/app/Http/Controllers/Frontend/BannerController.php
@@ -17,12 +17,12 @@ class BannerController extends Controller
         $currentDate = now()->toDateString(); // Get the current date in 'Y-m-d' format
 
         $banners = Banner::where('active', 1)
-            ->where(function ($query) use ($currentDate) {
-                $query->whereNull('published_start')->orWhereDate('published_start', '>=', $currentDate);
-            })
-            ->where(function ($query) use ($currentDate) {
-                $query->whereNull('published_end')->orWhereDate('published_end', '<=', $currentDate);
-            })
+            // ->where(function ($query) use ($currentDate) {
+            //     $query->whereNull('published_start')->orWhereDate('published_start', '>=', $currentDate);
+            // })
+            // ->where(function ($query) use ($currentDate) {
+            //     $query->whereNull('published_end')->orWhereDate('published_end', '<=', $currentDate);
+            // })
             ->defaultOrder()
             ->get();
         
