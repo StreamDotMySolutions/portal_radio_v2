045ffa6e54ac575dab7abdfeb2786f718c085ada
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-16 09:25:45 +0800

    [API] DirectoriesController , create Folder

--- api/app/Http/Controllers/Backend/DirectoryController.php
+++ api/app/Http/Controllers/Backend/DirectoryController.php
@@ -10,7 +10,23 @@ class DirectoryController extends Controller
     public function store(Request $request)
     {
         \Log::info($request);
+        $this->createCategoryWithChildren($request, null);
         return response()->json(['message' => 'Payload received']);
+    }
+
+    private function createCategoryWithChildren($data, $parent)
+    {
+        $category = Category::create(['name' => $data['name']]);
+
+        if ($parent) {
+            $category->appendToNode($parent)->save();
+        }
+
+        if (!empty($data['children'])) {
+            foreach ($data['children'] as $childData) {
+                $this->createCategoryWithChildren($childData, $category);
+            }
+        }
     }
 
 }
