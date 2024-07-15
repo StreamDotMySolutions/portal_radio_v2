c30e6b71df48b398d8e28b342851c8cb670054e7
StreamDotMy <streamdotmysolutions@gmail.com>
2024-07-15 10:59:28 +0800

    [Banner] fix expired banner

--- api/app/Http/Controllers/Frontend/BannerController.php
+++ api/app/Http/Controllers/Frontend/BannerController.php
@@ -6,39 +6,21 @@ use Illuminate\Http\Request;
 use App\Models\Banner;
 use Carbon\Carbon;
 
-
 class BannerController extends Controller
 {
     public function index()
     {
-
-        //$banners = Banner::query()->defaultOrder()->get()->toTree();
-        $banners = [];
-        
-        //$currentDate = now()->toDateString(); // Get the current date in 'Y-m-d' format
         $currentDate = Carbon::now();
 
         $banners = Banner::query()
             ->where('active', 1)
+            ->where(function ($query) use ($currentDate) {
+                $query->whereNull('published_end')
+                      ->orWhere('published_end', '>=', $currentDate);
+            })
             ->defaultOrder()
-            ->get()
-            ->map(function ($banner) use ($currentDate) {
-              
-                    if ($banner->published_end != null) {
-            
-                        $publishedEndDateTime = Carbon::parse($banner->published_end);
+            ->get();
 
-                        if($publishedEndDateTime->timestamp >= $currentDate->timestamp){
-                       
-                            return $banner; // only return if publsihed_end >= currentDate
-                        }
-                    } else {
-                        return $banner; // if published_end null, return as it is
-                    }
-                })
-                ->filter(); // remove empty array
-        
         return response()->json(['banners' => $banners]);
     }
-
 }
