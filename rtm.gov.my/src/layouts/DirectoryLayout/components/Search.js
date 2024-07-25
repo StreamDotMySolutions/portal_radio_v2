ef622dc6f9db1f7e201cd328da1adc62b4c005b6
StreamDotMy <streamdotmysolutions@gmail.com>
2024-07-25 10:22:39 +0800

    [Directory] search with icon

--- rtm.gov.my/src/layouts/DirectoryLayout/components/Search.js
+++ rtm.gov.my/src/layouts/DirectoryLayout/components/Search.js
@@ -1,31 +1,41 @@
 import React, { useState } from 'react';
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
 
-
-    const handleSearch = (e) => {
-        if (e.key === 'Enter') {
-          e.preventDefault();
-          // Navigate to the search results page with the query parameter
-          navigate(`/directories/search/${encodeURIComponent(searchTerm)}`);
+    const handleSearch = () => {
+        if (searchTerm.trim()) {
+            navigate(`/directories/search/${encodeURIComponent(searchTerm)}`);
         }
-      };
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleSearch();
+        }
+    };
 
     return (
         <div className="main">
-            <div className="form-group has-search">
-                <span className="fa fa-search form-control-feedback"></span>
+            <div className="input-group">
                 <input
                     type="text"
                     className="form-control"
                     placeholder="Carian Nama Pegawai"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyDown={handleSearch}
+                    onKeyDown={handleKeyDown}
                 />
+                <div className="input-group-append">
+                    <button className="btn btn-secondary" type="button" onClick={handleSearch}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
     );
