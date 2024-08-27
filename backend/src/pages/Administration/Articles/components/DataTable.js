56e399eb65f816de0597b2920949d3cad3558a0c
StreamDotMy <streamdotmysolutions@gmail.com>
2024-08-27 10:09:51 +0800

    [ArticleMAnagement] fix pagination to 100

--- backend/src/pages/Administration/Articles/components/DataTable.js
+++ backend/src/pages/Administration/Articles/components/DataTable.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Table, Button } from 'react-bootstrap'
+import { Table,Button } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 import useStore from '../../../store'
 import axios from '../../../../libs/axios'
@@ -14,88 +14,98 @@ import Ordering from './Ordering'
 const Index = () => {
     const store = useStore() // store management
     const { parentId } = useParams() // parentid
-    const defaultUrl = store.url + '/articles/node/' + parentId // set the index url to /api/articles/node/{parentId}
+    const url = store.url + '/articles/node/' + parentId // set the index url to /api/articles/node/{parentId}
     const [items, setItems] = useState([]) // data placeholder
-
+    
     // to get items data
-    //console.log(defaultUrl)
-    useEffect(() => {
-        // Fetch data with modified axios
-        axios({
-            method: 'get',
-            url: store.getValue('url') || defaultUrl // Use store.getValue('url') if available, otherwise defaultUrl
-        })
-        .then(response => {
-            //console.log(response)
-            setItems(response.data.articles) // get the data
-            store.setValue('refresh', false) // reset the refresh state to false
-        })
-        .catch(error => {
-            console.warn(error) // output to console
-        })
-    },
-    [
-        store.getValue('url'), // listen for URL changes by pagination click
-        store.getValue('refresh'), // listen when create / update / delete / search performed
-        parentId // when navigating to a different parent
-    ]); // useEffect()
+    useEffect( () => 
+        {
+            // modified axios to prepend Bearer Token on header
+            axios( 
+                {
+                    method: 'get', // method is GET
+                    url: store.getValue('url') ? store.getValue('url') : url
+                } 
+            )
+            .then( response => { // response block
+                //console.log(response)
+                setItems(response.data.articles) // get the data
+                store.setValue('refresh', false ) // reset the refresh state to false
+            })
+            .catch( error => { // error block
+                console.warn(error) // output to console
+            })
+      },
+        [
+            //store.getValue('url'), // listener when url changed by pagination click
+            store.getValue('refresh'), // listener when create / update / delete / search performed
+            parentId // when use navigate to parent
+        ] 
+
+    ) // useEffect()
+
+
 
     return (
         <div>
+    
             <CreateButton>
                 <CreateModal />
             </CreateButton>
             <Table>
                 <thead>
                     <tr>
-                        <th className='text-center' style={{ width: '20px' }}>
-                            <FontAwesomeIcon icon={['fas', 'hashtag']} />
-                        </th>
+                        <th className='text-center' style={{ 'width': '20px'}}><FontAwesomeIcon icon={['fas', 'hashtag']} /></th>
                         <th className='text-center'>Ordering</th>
-                        <th className='text-center' style={{ width: '100px' }}>Active ?</th>
+                        <th className='text-center' style={{ 'width': '100px'}}>Active ?</th>
                         <th>Title</th>
-                        <th style={{ width: '230px' }} className='text-center'>
-                            <FontAwesomeIcon icon={['fas', 'bolt']} />
-                        </th>
+                        <th  style={{ 'width': '230px'}} className='text-center'> <FontAwesomeIcon icon={['fas', 'bolt']} /></th>
                     </tr>
                 </thead>
+
                 <tbody>
-                    {items?.data?.map((item, index) => (
+                    {items?.data?.map((item,index) => (
+                        
                         <tr key={index}>
+                            <td><span className="badge bg-primary">{item.id}</span></td>
+                            <td className='text-center' style={{'width':'100px'}}>
+
+                                <Ordering id={item.id} direction='up' disabled={index === 0}/>
+                                {' '}
+                                <Ordering id={item.id} direction='down' disabled={index === items.data.length - 1 }/>
+                            
+                            </td>
+                            <td className='text-center'>{item.article_setting.active == 1 ? <FontAwesomeIcon className='text-success'  icon={['fas', 'check']} /> : <FontAwesomeIcon className='text-danger' icon={['fas', 'stop']} />  }</td>
                             <td>
-                                <span className="badge bg-primary">{item.id}</span>
-                            </td>
-                            <td className='text-center' style={{ width: '100px' }}>
-                                <Ordering id={item.id} direction='up' disabled={index === 0} />{' '}
-                                <Ordering id={item.id} direction='down' disabled={index === items.data.length - 1} />
-                            </td>
-                            <td className='text-center'>
-                                {item.article_setting.active === 1 ? 
-                                    <FontAwesomeIcon className='text-success' icon={['fas', 'check']} /> : 
-                                    <FontAwesomeIcon className='text-danger' icon={['fas', 'stop']} />
-                                }
-                            </td>
-                            <td>
-                                {item.descendants && item.descendants.length > 0 ? (
-                                    <FontAwesomeIcon className='me-2 text-warning' icon={['fas', 'fa-folder']} />
-                                ) : (
+                                {item.descendants && item.descendants.length > 0 ?
+                                    <FontAwesomeIcon className='me-2 text-warning' icon={['fas', 'fa-folder']} /> 
+                                :
                                     <FontAwesomeIcon className='me-2 text-secondary' icon={['fas', 'fa-file']} />
-                                )}
-                                <Link to={`/administration/articles/${item.id}`}>{item.title}</Link>
+                                }
+                                <Link to={`/administration/articles/${item.id}`}>{item.title}</Link>    
                             </td>
                             <td className='text-center'>
-                                <Link to={`/administration/articles/${item.id}`}>
-                                    <Button size='sm' variant='outline-secondary'>
-                                        <FontAwesomeIcon className='text-info' icon={['fas', 'fa-folder-plus']} />{' '}
-                                    </Button>
-                                </Link>{' '}
-                                <Link to={`/administration/articles-data/${item.id}`}>
-                                    <Button size='sm' variant='outline-primary'>
-                                        <FontAwesomeIcon icon={['fas', 'pen']} />{' '}
-                                    </Button>
-                                </Link>{' '}
-                                <EditModal id={item.id} />{' '}
-                                <DeleteModal id={item.id} />
+                               
+                                    <Link to={`/administration/articles/${item.id}`}>
+                                        <Button size='sm' variant='outline-secondary'>
+                                            <FontAwesomeIcon className='text-info'  icon={['fas', 'fa-folder-plus']} />{' '}
+                                        </Button>
+                                    </Link>
+                                {' '}
+                                   
+                                    <Link to={`/administration/articles-data/${item.id}`}>
+                                        <Button 
+                                            //disabled={item.descendants.length > 0 }
+                                            size='sm' variant='outline-primary'>
+                                            <FontAwesomeIcon icon={['fas', 'pen']} />{' '}
+                                        </Button>
+                                    </Link>
+                                    
+                                
+                                {' '}
+                                <EditModal id={item.id} />
+                                {' '}
+                                <DeleteModal id={item.id} /> 
                             </td>
                         </tr>
                     ))}
@@ -105,4 +115,4 @@ const Index = () => {
         </div>
     );
 };
-export default Index;
+export default Index
