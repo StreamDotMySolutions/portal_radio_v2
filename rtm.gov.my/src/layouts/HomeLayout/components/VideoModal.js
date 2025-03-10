f8925ba779ded17995ce080df2aaf0db8db4cb07
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-10 17:19:15 +0800

    [VideoBox] change Youtube to HLS

--- rtm.gov.my/src/layouts/HomeLayout/components/VideoModal.js
+++ rtm.gov.my/src/layouts/HomeLayout/components/VideoModal.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Hls from "hls.js";
@@ -7,7 +7,50 @@ const VideoModal = ({ embed_code, filename }) => {
   const serverUrl = process.env.REACT_APP_SERVER_URL;
   const videoSrc = `${serverUrl}/storage/vods/${embed_code}/playlist.m3u8`;
 
-  
+  const videoRef = useRef(null);
+  const modalRef = useRef(null);
+
+  const modalId = `videoModal-${embed_code}`;
+  const videoPlayerId = `videoPlayer-${embed_code}`;
+
+  useEffect(() => {
+    if (!embed_code) return; // Ensure embed_code is valid before proceeding
+
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play();
+      });
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = videoSrc;
+      video.addEventListener("loadedmetadata", () => {
+        video.play();
+      });
+    } else {
+      alert("Pelayar anda tidak menyokong HLS.");
+    }
+
+    const handleModalClose = () => {
+      video.pause();
+      video.currentTime = 0;
+    };
+
+    const modalElement = modalRef.current;
+    if (modalElement) {
+      modalElement.addEventListener("hidden.bs.modal", handleModalClose);
+    }
+
+    return () => {
+      if (modalElement) {
+        modalElement.removeEventListener("hidden.bs.modal", handleModalClose);
+      }
+    };
+  }, [filename]);
 
   return (
     <div className="container mt-5">
@@ -17,7 +60,7 @@ const VideoModal = ({ embed_code, filename }) => {
         type="button"
         className="btn btn-primary"
         data-bs-toggle="modal"
-        data-bs-target="#videoModal"
+        data-bs-target={`#${modalId}`}
       >
         {filename}
       </button>
@@ -25,10 +68,11 @@ const VideoModal = ({ embed_code, filename }) => {
       {/* Modal */}
       <div
         className="modal fade"
-        id="videoModal"
+        id={modalId}
         tabIndex="-1"
         aria-labelledby="videoModalLabel"
         aria-hidden="true"
+        ref={modalRef}
       >
         <div className="modal-dialog">
           <div className="modal-content">
@@ -42,7 +86,7 @@ const VideoModal = ({ embed_code, filename }) => {
             </div>
             <div className="modal-body">
               <p>{videoSrc} embed code is {embed_code}</p>
-              <video id="videoPlayer" width="100%" controls></video>
+              <video  id={videoPlayerId} ref={videoRef} width="100%" controls></video>
             </div>
           </div>
         </div>
