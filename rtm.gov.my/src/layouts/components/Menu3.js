c3a3e3931cde58f6394a4cffd47fb54d8f0468f1
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-11 11:38:05 +0800

    [Menu1] fix X logo and address

--- rtm.gov.my/src/layouts/components/Menu3.js
+++ rtm.gov.my/src/layouts/components/Menu3.js
@@ -166,7 +166,7 @@ const Menu3 = () => {
 
                 <div className='row ml-auto'>
                         <a style={{ color: "white" }} href="https://www.tiktok.com/@radiotelevisyenmalaysia?" className="nav-link font-weight-bold text-uppercase"><img src="/img/tiktok-xxl.png" className="img-fluid" alt="TikTok Logo" /></a>
-                        <a style={{ color: "white" }} href="https://twitter.com/rtm_malaysia?lang=en" className="nav-link font-weight-bold text-uppercase"><img src="/img/twitter.svg" className="img-fluid" alt="Twitter Logo" /></a>
+                        <a style={{ color: "white" }} href="https://x.com/rtm_malaysia?lang=en" className="nav-link font-weight-bold text-uppercase"><img src="/img/x.png" className="img-fluid" alt="Twitter Logo" /></a>
                         <a style={{ color: "white" }} href="https://www.facebook.com/RadioTelevisyenMalaysia/?locale=ms_MY" className="nav-link font-weight-bold text-uppercase"><img src="/img/facebook.svg" className="img-fluid" alt="Facebook Logo" /></a>
                         <a style={{ color: "white" }} href="https://www.instagram.com/rtm_malaysia/" className="nav-link font-weight-bold text-uppercase"><img src="/img/instagram.svg" className="img-fluid" alt="Instagram Logo" /></a>
                         <a style={{ color: "white" }} href="https://www.youtube.com/channel/UCF4KdUqyxJ5Cb0NTGhZXt9g" className="nav-link font-weight-bold text-uppercase"><img src="/img/youtube.png" className="img-fluid" alt="YouTube Logo" /></a>  
