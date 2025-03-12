9bc0a930f006252660dc92a12bf2a22d4c0ccbd6
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-12 08:50:58 +0800

    [HomeLayout] update component name

--- rtm.gov.my/src/layouts/HomeLayout/HomeLayout.js
+++ rtm.gov.my/src/layouts/HomeLayout/HomeLayout.js
@@ -5,13 +5,12 @@ import Menu3 from '../components/Menu3';
 import BannerCarousel from './components/BannerCarousel';
 import BannerProgramme from './components/BannerProgramme';
 import BannerProgramme2 from './components/BannerProgramme2';
-import Youtube from './components/Youtube';
-import Youtube2 from './components/Youtube2';
+
 import Footer from '../components/Footer';
 import Footer2 from '../components/Footer2';
-import Youtube3 from './components/Youtube3';
-import VideoModal from './components/VideoModal';
+
 import DesktopVideo from './components/DesktopVideo';
+import MobileVideo from './components/Youtube3';
 
 
 const HomeLayout = () => {
@@ -38,25 +37,12 @@ const HomeLayout = () => {
                 <BannerProgramme2 />
             </div>
 
-            {/* <div className="d-none d-md-block" id="youtube-desktop">
-                <Youtube />
-            </div> */}
-
-            <div className="d-none d-md-block" id="youtube-desktop">
+            <div className="d-none d-md-block" id="video-desktop">
                 <DesktopVideo />
             </div>
 
-
-            {/* <div className="d-none d-md-block" id="youtube-desktop">
-                <div className="container-fluid" style={{ padding: '50px', background: 'linear-gradient(180deg, #103875 0%, #2f57ce 100%)' }}>
-                    <VideoModal />
-                </div>
-            </div> */}
-
-            
-            
-            <div className="d-md-none" id="youtube-mobile">
-               <Youtube3 />
+            <div className="d-md-none" id="video-mobile">
+               <MobileVideo />
             </div>
 
             <div className="d-none d-md-block" id="footer-desktop">
