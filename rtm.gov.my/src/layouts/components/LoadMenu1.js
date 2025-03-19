88452839a6d9a0af20180e66a1bfae27aff6d2e4
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-19 15:37:49 +0800

    [LoadMenu] add icon open new window

--- rtm.gov.my/src/layouts/components/LoadMenu1.js
+++ rtm.gov.my/src/layouts/components/LoadMenu1.js
@@ -111,7 +111,7 @@ function LoadMenu1({id}) {
         
         <li className='nav-item'>
           {settings.redirect_url ?
-            <NavLink className="nav-link" to={`${settings.redirect_url}`}>{title}</NavLink>
+            <NavLink className="nav-link" to={`${settings.redirect_url}`}>{title} {' '}<i class="fa-solid fa-up-right-from-square"></i></NavLink>
           :
             <NavLink className="nav-link" to={`/contents/${id}`}>{title}</NavLink>
           }
