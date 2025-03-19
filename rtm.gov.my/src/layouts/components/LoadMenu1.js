c5ca50eb7f535d63752b79b24abc7d4001089f2f
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-19 15:47:49 +0800

    [LoadMenu] add icon open new window

--- rtm.gov.my/src/layouts/components/LoadMenu1.js
+++ rtm.gov.my/src/layouts/components/LoadMenu1.js
@@ -93,7 +93,7 @@ function LoadMenu1({id}) {
                   <span
                     dangerouslySetInnerHTML={{
                       __html: article.article_setting.redirect_url
-                      ? `${truncateTitleWithBreaks(article.title, 3)} <span class="text-muted">[ external link ]</span>`
+                      ? `${truncateTitleWithBreaks(article.title, 3)} <i class="fa-solid fa-up-right-from-square"></i>`
                       : truncateTitleWithBreaks(article.title, 3),
                     }}
                 />
