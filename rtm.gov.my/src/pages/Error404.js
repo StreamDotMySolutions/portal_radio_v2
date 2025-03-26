bcef6880a1148c39cb1b359143dcc910b009133e
Azril Nazli Alias <streamdotmysolutions@gmail.com>
2025-03-26 19:44:24 +0800

    [FRONTEND] update error 404 page

--- rtm.gov.my/src/pages/Error404.js
+++ rtm.gov.my/src/pages/Error404.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Error = () => {
-    return (
-        <>
-            <h1>Error 404</h1>
-        </>
-    )
-} 
+const Error404 = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      />
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="text-center">
+          <div className="display-1 fw-bold text-danger">RALAT 404</div>
+          <p className="fs-4 mb-4">Halaman yang dicari tidak ditemui!</p>
+          <a href="https://www.rtm.gov.my/" className="btn btn-primary">
+            Halaman Utama
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Error
+export default Error404;
